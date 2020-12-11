87be2695d0e2e533b253cea08b508be305d8111f
bladonjay <bladonjay@gmail.com>
2020-12-10 22:22:16 -0500

    Update GetLFPBand.m
    Now GetLFPBand has a wavelet option

--- GetLFPBand.m
+++ GetLFPBand.m
@@ -1,12 +1,19 @@
-function [Phase,Amplitude,Filtered] = GetLFPBand(LFPdata,LFPts,band,correction)
+function [Phase,Amplitude,Filtered] = GetLFPBand(LFPdata,LFPts,band,method)
 % function [Phase,Amplitude,Filtered] = GetLFPBand(LFPdata,LFPts,band,correction)
 % GetLFPBands pulls the phases and amplitudes of your data in the freq band
-% you're looking for.  Uses a butterworth filter then takes the hilbert
+% you're looking for.  
+
+
+% methods 0 and 1 Use a butterworth filter then takes the hilbert
 % transorm.  Does not threshold the dataset. This is a rough approximation
 % of the dataset.  Data should come in at 1000 Hz.
 
 
-% butterworth filter and hilbert transform
+
+% method 2 takes the wavelet, finds the maximum amplitude at each time
+% point and uses the phase from that frequency.  You will get the filtered
+% signal as well
+
 
 
 if ~exist('band','var')
@@ -17,10 +24,10 @@ else
 end
 
 % if no corr var or if its neither 0 or 1
-if ~exist('correction','var')
-    correction=1;
-elseif correction~=0 && correction ~=1
-    correction=1;
+if ~exist('method','var')
+    method=1;
+elseif method~=0 && method ~=1
+    method=1;
 end
 
 tempdt=min(diff(LFPts));
@@ -28,6 +35,8 @@ tempdt=min(diff(LFPts));
 dt=.001;
 fNQ=1/2/dt;
 % get params for bandpass filter
+
+if method <2
 thetapass=freq.*(2*dt);
 
 % use a 3 pole butterworth filter
@@ -35,12 +44,13 @@ thetapass=freq.*(2*dt);
 
 Filtered=filtfilt(a,b,LFPdata);
 
+% for other filtereing methods
 %{
 % or a digital bandpass iir of order 20
 bpFilt = designfilt('bandpassiir','FilterOrder',20, ...
          'HalfPowerFrequency1',band(1),'HalfPowerFrequency2',band(2), ...
          'SampleRate',1000);
-Filtered2=filtfilt(bpFilt,LFPdata);
+Filtered=filtfilt(bpFilt,LFPdata);
      
      
 % or a first order fiir this takes wayyyyyy long because its order 8000
@@ -48,17 +58,28 @@ filtorder=3*fix(band(1)/dt);
 fi=[0 (1-.15)*band(1)/fNQ band(1)/fNQ band(2)/fNQ (1+.15)*band(2)/fNQ 1];
 mi=[0  0                  1           1            0                  0] ;
 filtwts = firls(filtorder,fi,mi);             % get FIR filter coefficients
-Filtered2 = filtfilt(filtwts,1,LFPdata);
+Filtered = filtfilt(filtwts,1,LFPdata);
 %}
 % get phase and amplitude using the hilbert trnaform
 thetaparts=hilbert(Filtered);
 % these are the parts
 Amplitude=abs(thetaparts);
 Phase=angle(thetaparts);
-
+elseif method == 2
+    % wavelet the data in 10 linear increments within your band
+    wname='cmor16-.5'; % a 16 x envelope of a morlet at 0.5 hz central frequency
+    freqs=linspace(band(1),band(2),10); % log spaced vector to ~250 hz
+    bases=500./freqs;
+    wtband=cwt(LFPdata,bases,wname);
+    allAmps=abs(wtband);
+    AllPhases=angle(wtband);
+    [Amplitude,maxinds]=max(allAmps,[],1);
+    fullinds=sub2ind(size(allPhases),maxinds,1:size(allPhases,2));
+    Phase=allPhases(fullinds);
+end
 % and here we correct for the asymmetry of the oscillation phases, as it
 % can be saw-toothed.
-if correction==1
+if method==1
     [cdfphase,newphase]=sort(Phase);
     [mycdf,x]=ecdf(cdfphase);
     [~,ib]=sort(newphase);
